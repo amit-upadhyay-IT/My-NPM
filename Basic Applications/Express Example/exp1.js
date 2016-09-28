dc77833d151a1d0f450271ed59bc461e8a27f350
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-09-29 04:22:16 +0530

    express.js example added

---
+++ Basic Applications/Express Example/exp1.js
@@ -0,0 +1,5 @@
+var exp = require("express")().listen("5555");
+
+// so in this one line of code we import express lib, we create and instance
+// of it and the createServer insternally gets called and we start listening
+// on the port number 5555.
