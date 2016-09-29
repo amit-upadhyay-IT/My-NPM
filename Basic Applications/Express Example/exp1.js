ba18f07f1dc63964785522fdaeee5bcec57a4423
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-09-29 11:00:56 +0530

    split express program in multiple lines:

--- Basic Applications/Express Example/exp1.js
+++ Basic Applications/Express Example/exp1.js
@@ -1,4 +1,8 @@
-var exp = require("express")().listen("5555");
+var exp = require("express")();
+
+exp.listen("5555", function(){
+    console.log("Server is on at 5555");
+});
 
 // so in this one line of code we import express lib, we create and instance
 // of it and the createServer insternally gets called and we start listening
