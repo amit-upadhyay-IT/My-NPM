52f49a1c25a7c6457668650595306c6e2eee5550
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-09-29 15:42:33 +0530

    added downloading feature (./download page) | added ./data page

--- Basic Applications/Express Example/exp1.js
+++ Basic Applications/Express Example/exp1.js
@@ -2,6 +2,19 @@ var exp = require("express");
 
 var app = exp();
 
+app.get("/data", function(req, res){
+    res.send("You called data");
+}); // this is a very powerful method I'm calling, basically I'm
+// that whenever any get request (not the post,put,delete request) with
+// ./data then this function of mine has to be executed
+
+// similarly we can make it for downloading something
+app.get("/download", function(req, res){
+    res.download("./first.js", function(){
+        console.log("download is over");
+    });
+});
+
 app.listen("5555", function(){
     console.log("Server is on at 5555");
 });
