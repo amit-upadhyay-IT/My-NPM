967e3137b012ddea091725230ca9bf0844f5c5e0
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-01 18:33:37 +0530

    changed custom module app

--- Basic Applications/custom/app.js
+++ Basic Applications/custom/app.js
@@ -1,14 +1,6 @@
 // some mathematical calculations will be performed in some other file and
 // through this file we will be calling those functions written in other file.
 
-function addition(x, y){
-    console.log("Addition of "+x+" and "+y+"is " +(x+y));
-}
-
-function multiplication(x, y){
-    console.log("Multiplication of "+x+" and "+y+"is " +(x*y));
-}
-
 addition(3, 7);
 
 multiplication(2, 5);
---
+++ Basic Applications/custom/maths.js
@@ -0,0 +1,8 @@
+module.exports.add = function (x, y){
+    console.log("Addition of "+x+" and "+y+"is " +(x+y));
+}
+
+module.exports.mult = function (x, y){
+    console.log("Multiplication of "+x+" and "+y+"is " +(x*y));
+}
+
