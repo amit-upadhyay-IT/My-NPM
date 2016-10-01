c5b05b623c30d84c05a58da7b72862af403887ba
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-01 18:59:00 +0530

    updated custom app

--- Basic Applications/custom/app.js
+++ Basic Applications/custom/app.js
@@ -1,7 +1,7 @@
 // some mathematical calculations will be performed in some other file and
 // through this file we will be calling those functions written in other file.
 
-var myMaths = require('./maths.js');
+var myMaths = require('maths.js');
 
 myMaths.add(3, 7);
 
