f75669166dc51b01c304f27c222dd49f29cbdb5e
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-08 17:19:07 +0530

    handlebars updated

--- TemplatesApps/handlebars/routes/routes.js
+++ TemplatesApps/handlebars/routes/routes.js
@@ -1,12 +1,14 @@
 
-exports.loginPageHandler = function (req, res){// this is mapping the home page
+exports.loginPageHandler = function(req, res){
+	res.render('login.handlebars', {});
+}//loginPageHandler
 
-    //req.session.destroy();
-    res.render('login.handlebars', {});
+exports.logoutHandler = function(req, res){
+	req.session.destroy();
+	res.render('login.handlebars', {LOGGEDIN:false});
+}//logoutHandler
 
-}
-
-exports.landingPageHandler = function (req, res){
+exports.landingPageHandler = function(req, res){
 
     // it is the handler for the page which comes after user logs in
     // if by that time if the user has provided a name (that name will be present in session)
@@ -16,43 +18,48 @@ exports.landingPageHandler = function (req, res){
     // will be nm. So the query parameter of request which is going to be get request so login request
     // if you remember is a get request. So whatever the parameter nm will have then that will be
     // coming to you as req.query.nm and that value we are populating in person variable
+	// user comes to this handler after login, set loggedin variable in session to true.
+	req.session.loggedin = true;
 
-    var person;
-    if (res.session.userName)
-    {
-        person = req.session.userName;
-    }
-    else{
-         person = req.query.nm;
-    }
+	var person;
+	if (req.session.userName){   //session store has userName
+		console.log("User Name already in session. It is " + req.session.userName);
+		person = req.session.userName;
+	}else{ //session store does NOT have userName
+		// read username from req.query and keep into the session store
+		person = req.query.nm;
+		req.session.userName = person;
+		console.log("User Name does not exist in session. Hence storing it in session store " + person);
+	}
 
-    // new we are rendering the landingpage.
+	res.render('landingpage.handlebars', {welcomeMessage:person, 
+										LOGGEDIN:req.session.loggedin});
+}//landingPageHandler
 
-    res.render('landingpage.handlebars', {welcomeMessage:person});
-
-    // so server side code for the landingPageHandler is only this
-
-}
 exports.cityPageHandler = function(req, res){
+	var interestValue = req.body.interest;
+	var cityNameValue, taglineValue;
+	console.log("received interestValue  as " + interestValue);
+	var imageArray = [];
 
-    var cityname = req.params.city;
-    var person;
-    var headlineValue;
+	if (interestValue === 'history'){
+		cityNameValue = 'Rome';
+		taglineValue = 'The city of earliest civilization';
+		imageArray = [1,2];
+	}else if (interestValue === 'fashion'){
+		cityNameValue = 'Paris';
+		taglineValue = 'The fashion capital of the world';
+		imageArray = [1,2,3];
+	}else if (interestValue === 'finance'){
+		cityNameValue = 'New York';
+		taglineValue = 'The business capital of the world';
+		imageArray = [1,2,3,4,5,6];
+	}
+	
+	res.render('city.handlebars', {cityName:cityNameValue, 
+						tagline: taglineValue, 
+						welcomeMessage:req.session.userName,
+						imageArray:imageArray,
+						LOGGEDIN:req.session.loggedin});
 
-    if (cityname === 'newyork'){
-         titleValue = "New York";
-         headlineValue = "Business capital of the world";
-    } else if (cityname === 'london'){
-        titleValue = "London";
-        headlineValue = "City of the Thames";
-    } else if (cityname === 'newdelhi'){
-         titleValue = "New Delhlli;i"
-         headlineValue = "Capital city of India";
-    } else if (cityname === 'paris'){
-        titleValue = "Paris ";
-        headlineValue = "Fashion capital of the world";
-    }
-    res.render('city.ejs', {title:titleValue, headline: headlineValue});
-};
-
-// so we have structure of all three page handlers.
+}//cityPageHandler
